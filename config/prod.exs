cabfb5b56ec8dbe553e259581fc0fe95d33e9e24
Trent Ogren <tedwardo2@gmail.com>
2016-03-14 10:32:59 -0400

    add real hostname

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :chirp_awareness, ChirpAwareness.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "104.236.250.122", port: 3001],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
